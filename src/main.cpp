a1ea2143e614b836046ee8a65716717e00111831
AlexLikeiOS <71228712+AlexLikeiOS@users.noreply.github.com>
2022-02-13 17:10:06 +0300

    Make main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UAV_WiFi_detection
+//
+//  Created by Alex Shchelochkov on 13.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Alex!\n";
+    return 0;
+}
