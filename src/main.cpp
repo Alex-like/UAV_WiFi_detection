1dad2f553df5cf6cfa4ea4d701c2fd5fb97f11e3
Alex-like <sky_line.5@rambler.ru>
2022-02-16 20:45:24 +0300

    Modify .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -32,20 +32,21 @@ void readFromLog() {
 
 void readFromPcap() {
     string path = "/Users/alexshchelochkov/Desktop/STC/UAV_WiFi_detection/data/drones/3dr_solo/dsss/frames.pcap";
-    char errbuff[PCAP_ERRBUF_SIZE];
-    pcap_t * pcap = pcap_open_offline(path.c_str(), errbuff);
-    struct pcap_pkthdr *header;
-    const u_char *data;
-    
-    u_int packetCount = 0;
-    while( int returnValue = pcap_next_ex(pcap , &header, &data) >= 0) {
-        cout << "test";
-        printf("Packet number %i\n", ++packetCount);
-        printf("Packet size: %d bytes\n", header->len);
-        if (header->len != header->caplen)
-            printf("Warning! Packet size different from capture size: %u bytes\n", header->len);
-        printf("Epoch time: %ld:%d seconds\n\n\n", header->ts.tv_sec, header->ts.tv_usec);
-    }
+//    char errbuff[PCAP_ERRBUF_SIZE];
+//    pcap_t * pcap = pcap_open_offline(path.c_str(), errbuff);
+//    struct pcap_pkthdr *header;
+//    const u_char *data;
+//
+//    u_int packetCount = 0;
+//    while( int returnValue = pcap_next_ex(pcap , &header, &data) >= 0) {
+//        cout << "test";
+//        printf("Packet number %i\n", ++packetCount);
+//        printf("Packet size: %d bytes\n", header->len);
+//        if (header->len != header->caplen)
+//            printf("Warning! Packet size different from capture size: %u bytes\n", header->len);
+//        printf("Epoch time: %ld:%d seconds\n\n\n", header->ts.tv_sec, header->ts.tv_usec);
+//    }
+//    pcap_close(pcap);
 }
 
 int main(int argc, const char * argv[]) {
