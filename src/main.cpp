4da3a9cf379fc7bbf573d35446aa49d51bcca228
Alex-like <sky_line.5@rambler.ru>
2022-03-28 09:17:03 +0300

    Separate all functions and add worker for single file

--- src/main.cpp
+++ src/main.cpp
@@ -17,75 +17,47 @@
 
 using namespace std;
 
-void readFromLog(string path) {
-    
-    vector<LogFrame> frames;
-    readFromFile(path, frames);
-    
-//    for (LogFrame frame : frames) {
-//        cout << frame.toString() << '\n';
-//        cout << frame.getTAAndRA() << "\n\n";
-//    }
-    
-//    Statistics stat(frames);
-//    cout << stat.toString() << '\n';
-    
-//    map<string, u_int64_t> types;
-//    for (LogFrame frame : frames) {
-//        if (frame.getType().has_value()) {
-//            if (types.find(frame.getType().value()) == types.end()) {
-//                types[frame.getType().value()] = 0;
-//            }
-//            types[frame.getType().value()]++;
-//        }
-//    }
-//    for (auto &type : types) {
-//        cout << type.first << " : " << type.second << '\n';
-//    }
-    
-//    Graph graph = Graph();
-//    for (LogFrame frame : frames) {
-//        graph.addEdge(frame.getTA(), frame.getRA());
-//    }
-//    cout << graph.toString() << "\n";
-    
-//    GroupedGraph graph = GroupedGraph();
-//    for (LogFrame frame : frames) {
-//        if (frame.getType() == "Management/Beacon") {
-//            if (frame.getSSID().has_value() && frame.getTA().has_value()) {
-//                graph.addGroup(frame.getSSID().value(), frame.getTA().value());
-//            }
-//        }
-//        if (frame.getTA().has_value() && frame.getRA().has_value()) {
-//            graph.addEdge(frame.getTA().value(), frame.getRA().value());
-//        }
-//    }
-//    cout << graph.toString() << '\n';
-//
-//    map<u_int64_t, optional<string>> macAndSsid;
-//    for (LogFrame frame : frames) {
-//        if (frame.getTA().has_value()) {
-//            macAndSsid[frame.getTA().value()] = frame.getSSID();
-//        }
-//    }
-//
-//    set<string> macs;
-//    for (LogFrame frame : frames) {
-//        getAddressesFromStr(frame.getData(), macs);
-//    }
-//
-//    Classifier classifier = Classifier(graph);
-//    vector<Object> objs;
-//    for (string mac : macs) {
-//        u_int64_t mac_v = macToHex(mac);
-//        optional<Object> obj = classifier.classify(mac_v, macAndSsid[mac_v]);
-//        if (obj.has_value()) {
-//            objs.push_back(obj.value());
-//        }
-//    }
-//    for (Object obj : objs) {
-//        cout << obj.toString() << '\n';
-//    }
+void getStatistics(vector<LogFrame> &frames) {
+    Statistics stat(frames);
+    cout << stat.toString() << '\n';
+}
+
+void countFrameTypes(vector<LogFrame> &frames) {
+    map<string, u_int64_t> types;
+    for (LogFrame frame : frames) {
+        if (frame.getType().has_value()) {
+            if (types.find(frame.getType().value()) == types.end()) {
+                types[frame.getType().value()] = 0;
+            }
+            types[frame.getType().value()]++;
+        }
+    }
+    for (auto &type : types) {
+        cout << type.first << " : " << type.second << '\n';
+    }
+}
+
+void makeSimpleGraph(vector<LogFrame> &frames) {
+    Graph graph = Graph();
+    for (LogFrame frame : frames) {
+        graph.addEdge(frame.getTA(), frame.getRA());
+    }
+    cout << graph.toString() << "\n";
+}
+
+void getNetworks(vector<LogFrame> &frames) {
+    GroupedGraph graph = GroupedGraph();
+    for (LogFrame frame : frames) {
+        if (frame.getType() == "Management/Beacon") {
+            if (frame.getSSID().has_value() && frame.getTA().has_value()) {
+                graph.addGroup(frame.getSSID().value(), frame.getTA().value());
+            }
+        }
+        if (frame.getTA().has_value() && frame.getRA().has_value()) {
+            graph.addEdge(frame.getTA().value(), frame.getRA().value());
+        }
+    }
+    cout << graph.toString() << '\n';
 }
 
 void getAddressesFromFrames(vector<LogFrame> &frames) {
@@ -157,6 +129,16 @@ void classifyAllMacFromFrames(vector<LogFrame> &frames) {
     }
 }
 
+void workWithDefiniteFile(function<void(vector<LogFrame> &)> action) {
+    string path;
+    cout << "Введите путь до файла:\n";
+    cin >> path;
+    cout << '\n';
+    vector<LogFrame> frames;
+    readFromFile(path, frames);
+    
+}
+
 void workWithAllFiles(function<void(vector<LogFrame> &)> action) {
     string paths[] = {
     };
@@ -170,11 +152,7 @@ void workWithAllFiles(function<void(vector<LogFrame> &)> action) {
 }
 
 int main(int argc, const char * argv[]) {
-//    string path;
-//    cout << "Введите путь до файла:\n";
-//    cin >> path;
-//    cout << '\n';
-//    readFromLog(path);
+//    workWithDefiniteFile(getStatistics);
     workWithAllFiles(classifyAllMacFromFrames);
     
     return 0;
